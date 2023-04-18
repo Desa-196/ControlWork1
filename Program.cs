76a1f4a84aa06fcf2ca9962b93530cdf4f2f0f97
Ponomarev Denis <desa-196@yandex.ru>
2023-04-18 14:10:02 +0300

    Написал функцию ожидающую ввода числа

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Функция выводит сообщение message и ожидает ввода числа с проверкой, что ввели число.
+int read_int_from_console(string message)
+{
+    int console_int = 0;
+
+    Console.Write($"{message}: ");
+
+    while(true)
+    {
+        if( int.TryParse(Console.ReadLine(), out console_int ) )
+        {
+            break;
+        }
+        else
+        {
+            Console.Write("Введено некорректное число, повторите попытку ввода:");
+        }
+    }
+    return console_int;
+
+}
